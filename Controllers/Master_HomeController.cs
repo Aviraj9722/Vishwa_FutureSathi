3a9a4563fc815a099797aa180249bc74f1885772
Shadab shaikh <shadabshaikh313313@gmail.com>
2023-11-18 18:42:40 +0530

    First Push-Shadab

---
+++ Controllers/Master_HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FutureSathi.Controllers
+{
+    public class Master_HomeController : Controller
+    {
+        // GET: Master_Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Services/UserRepo.cs
@@ -0,0 +1,50 @@
+﻿using FutureSathi.IServices;
+using FutureSathi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FutureSathi.Services
+{
+    public class UserRepo : IUser
+    {
+        FutureSathiEntities ctx = new FutureSathiEntities();
+        public bool Login(tblUser obj)
+        {
+            return true;
+        }
+
+     
+
+        public bool Signin(UserClass obj)
+        {
+            if (obj != null)
+            {
+                try
+                {
+                    tblUser U = new tblUser();
+                    U.First_Name = obj.First_Name;
+                    U.Last_Name = obj.Last_Name;
+                    U.Gender_id = obj.Gender_id;
+                    U.Contact = obj.Mobile_No;
+                    U.Email = obj.Email;
+                    U.Password = obj.Password;
+
+
+                    ctx.tblUsers.Add(U);
+                    ctx.SaveChanges();
+
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false; 
+
+                }
+            }
+
+            return false;
+        }
+    }
+}
